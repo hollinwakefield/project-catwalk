40ec48231eb9e5489c6a1dc5c9466a53c8f786a1
Kate Jeon <katehyerinjeon@gmail.com>
2021-06-10 08:22:38 +0900

    Fixed add to cart error

---
+++ client/src/components/productOverview/addToCart.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const AddToCartArea = styled.div`
+  grid-area: AddToCart;
+  background: palevioletred;
+`;
+
+const AddToCart = () => <AddToCartArea>Add to cart</AddToCartArea>;
+
+export default AddToCart;
