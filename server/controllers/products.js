641672abfd9f853ce97be3ea40c1000dfa554e14
ChhuongLe <chhuongcle@gmail.com>
2021-06-09 22:29:57 -0700

    get related items object to be passed in from API

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -17,13 +17,25 @@ router.get('/:productId', (req, res) => {
 // get all styles - Kate
 
 // get related items - Chhuong
+// will send back an array of related items' unique ID
+// IDEA CHANGE - Get productInformation of each related item
 router.get('/:productId/related', (req, res) => {
   const { productId } = req.params;
   productAPI.getRelatedItems(productId, (err, data) => {
     if (err) {
       res.send(err);
     } else {
-      res.status(200).send(data);
+      const relatedItems = [];
+      data.map((id) => productAPI.getProductInfo(id, (err2, data2) => {
+        if (err2) {
+          res.send(err2);
+        } else {
+          relatedItems.push(data2);
+          if (data.length === relatedItems.length) {
+            res.send(relatedItems);
+          }
+        }
+      }));
     }
   });
 });
