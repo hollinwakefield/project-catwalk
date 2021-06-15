5dd0824fc53c467b3d9434617af8bfe2310af294
Kate Jeon <katehyerinjeon@gmail.com>
2021-06-15 13:46:04 +0900

    Implemented react component render tests for product overview

---
+++ client/src/testSuite/ProductOverviewTest/ProductOverview.test.js
@@ -0,0 +1,71 @@
+/* eslint-disable react/jsx-filename-extension */
+import React, { useState } from 'react';
+import { render, screen } from '@testing-library/react';
+// import App from '../../components/App';
+import ProductOverview from '../../components/ProductOverview/ProductOverview';
+import ImageGallery from '../../components/ProductOverview/ImageGallery';
+import ProductInfo from '../../components/ProductOverview/ProductInfo';
+import StyleSelector from '../../components/ProductOverview/StyleSelector';
+import AddToCart from '../../components/ProductOverview/AddToCart';
+import Description from '../../components/ProductOverview/Description';
+import dummyData from './dummyData';
+
+const { product, styles, cart } = dummyData;
+const style = styles.results[0];
+// const [style, setStyle] = useState(styles.results[0]);
+
+// 0. App
+// describe('App', () => {
+//   test('renders App component', () => {
+//     render(<App />);
+//     screen.debug();
+//   });
+// });
+
+// 1. ProductOverview
+describe('ProductOverview', () => {
+  test('renders ProductOverview component', () => {
+    render(<ProductOverview product={product} styles={styles} cart={cart} />);
+    screen.debug();
+  });
+});
+
+// 2. ImageGallery
+describe('ImageGallery', () => {
+  test('renders ImageGallery component', () => {
+    render(<ImageGallery style={style} />);
+    screen.debug();
+  });
+});
+
+// 3. ProductInfo
+describe('ProductInfo', () => {
+  test('renders ProductInfo component', () => {
+    render(<ProductInfo product={product} style={style} rating={3.5} />);
+    screen.debug();
+  });
+});
+
+// 4. StyleSelector
+// describe('StyleSelector', () => {
+//   test('renders StyleSelector component', () => {
+//     render(<StyleSelector styles={styles} style={style} setStyle={setStyle} />);
+//     screen.debug();
+//   });
+// });
+
+// 5. AddToCart
+describe('AddToCart', () => {
+  test('renders AddToCart component', () => {
+    render(<AddToCart style={style} />);
+    screen.debug();
+  });
+});
+
+// 6. Description
+describe('Description', () => {
+  test('renders Description component', () => {
+    render(<Description product={product} />);
+    screen.debug();
+  });
+});
