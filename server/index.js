ff7ba3de99f2e1548856090ea58be630d4ff447d
Steven Lam <stl026@ucsd.edu>
2021-06-17 17:51:10 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, '../public/dist')));
-app.use(bodyParser());
+app.use(bodyParser.json());
 
 app.use('/products', products);
 app.use('/reviews', reviews);
