8cd4ad305ae5086bbd143d36bb0d47274d4bf488
ChhuongLe <chhuongcle@gmail.com>
2021-06-19 15:53:34 -0700

    Change this name

---
+++ client/src/components/Banner/Banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+import logo from './cat-face.jpg';
+
+const Banner = () => (
+  <div>
+    <img src={logo} alt="nothin here" aria-label="banner" />
+  </div>
+);
+
+export default Banner;
