d26111cf7c1c18d0b22de6fad04fe8fa5cbab593
Chhuong Le <77046302+ChhuongLe@users.noreply.github.com>
2021-06-19 14:22:11 -0700

    Update App.jsx

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -9,7 +9,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      productId: 25169,
+      productId: 25167,
       product: null,
       styles: null,
       cart: null,
